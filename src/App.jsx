d3f7fb3b386e80ff629929aa85c15f0644fad779
Breno Rocha <brenopenarocha2@gmail.com>
2024-02-07 16:22:21 -0300

    selectSquare function

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,35 @@ import Game from './components/Game.jsx';
 
 function App() {
 
-  /*
-  const showGame = () => {
+  const selectSquare = (id) => {
+    let boardSquare = document.getElementById(id);
 
+
+    //console.log(boardSquare.innerHTML);
+    if(boardSquare.innerHTML == "") {
+      let symbol = document.createElement("p");
+      symbol.classList.add("symbol");
+      symbol.innerText = "X";
+
+      boardSquare.appendChild(symbol);
+    }
+    else {
+      console.log("you already clicked here!");
+    }
   }
-  */
 
   const handleClick = () => {
-    /*
     let startMenu = document.getElementById("start-menu");
+    let gameUI = document.getElementById("game-ui");
 
     startMenu.style.display = "none";
-    showGame();
-    */
+    gameUI.style.display = "flex";
   }
 
   return (
     <div className="wrapper">
       <StartMenu handleClick= { handleClick } />
-      <Game />
+      <Game selectSquare={ selectSquare } />
     </div>
   )
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import '../styles/Game.css';
 
-const Game = () => {
+const Game = ({ selectSquare }) => {
     return (
-        <div className="game-ui">
+        <div id="game-ui" className="game-ui">
             <div className="player-symbol">
                 <h2>You</h2>
                 <p className="symbol">X</p>                
             </div>
 
             <div className="game-board">
-                <div className="board-square"></div>
-                <div className="board-square"></div>
-                <div className="board-square"></div>
-                <div className="board-square"></div>
-                <div className="board-square"></div>
-                <div className="board-square"></div>
-                <div className="board-square"></div>
-                <div className="board-square"></div>
-                <div className="board-square"></div>
+                <div id="board-square-1" className="board-square" onClick={ () => selectSquare("board-square-1") }></div>
+                <div id="board-square-2" className="board-square" onClick={ () => selectSquare("board-square-2") }></div>
+                <div id="board-square-3" className="board-square" onClick={ () => selectSquare("board-square-3") }></div>
+                <div id="board-square-4" className="board-square" onClick={ () => selectSquare("board-square-4") }></div>
+                <div id="board-square-5" className="board-square" onClick={ () => selectSquare("board-square-5") }></div>
+                <div id="board-square-6" className="board-square" onClick={ () => selectSquare("board-square-6") }></div>
+                <div id="board-square-7" className="board-square" onClick={ () => selectSquare("board-square-7") }></div>
+                <div id="board-square-8" className="board-square" onClick={ () => selectSquare("board-square-8") }></div>
+                <div id="board-square-9" className="board-square" onClick={ () => selectSquare("board-square-9") }></div>
             </div>
             
             <div className="player-symbol">

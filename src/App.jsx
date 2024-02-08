1d58156084d2f929950979cb4a494fae75f1598b
Breno Rocha <brenopenarocha2@gmail.com>
2024-02-08 12:01:45 -0300

    Finished checkBoard functions

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,92 @@ import Result from "./components/Result.jsx";
 let board = ["", "", "", "", "", "", "", "", ""];
 
 
+
 function App() {
 
+  const openResult = (value) => {
+    let gameUI = document.getElementById("game-ui");
+    let result = document.getElementById("result");
+    let resultText = document.getElementById("result-text");
+    gameUI.style.display = "none";
+    result.style.display = "block";
+
+    if(value === "X") {
+      resultText.innerText = "You're a Winner!";
+    }
+    else if (value === "O") {
+      resultText.innerText = "You Lost!";
+    }
+    else {
+      resultText.innerText = "Tie!";
+    }
+  }
+
+
+  const checkRows = () => {
+    if(board[0] === "X" && board[1] === "X" && board[2] === "X" ||
+      board[3] === "X" && board[4] === "X" && board[5] === "X" ||
+      board[6] === "X" && board[7] === "X" && board[8] === "X") {
+        console.log("Row completed");
+        return "win";
+    }
+
+    else if(board[0] === "O" && board[1] === "O" && board[2] === "O" ||
+      board[3] === "O" && board[4] === "O" && board[5] === "O" ||
+      board[6] === "O" && board[7] === "O" && board[8] === "O") {
+        console.log("Row completed");
+        return "loss";
+    }
+
+  }
+
+
+  const checkColumns = () => {
+    if(board[0] === "X" && board[3] === "X" && board[6] === "X" ||
+      board[1] === "X" && board[4] === "X" && board[7] === "X" ||
+      board[2] === "X" && board[5] === "X" && board[8] === "X") {
+        console.log("Column completed");
+        return "win";
+    }
+
+    else if(board[0] === "O" && board[3] === "O" && board[6] === "O" ||
+      board[1] === "O" && board[4] === "O" && board[7] === "O" ||
+      board[2] === "O" && board[5] === "O" && board[8] === "O") {
+        console.log("Column completed");
+        return "loss";
+    }
+  }
+
+
+  const checkDiagonal = () => {
+    if(board[0] === "X" && board[4] === "X" && board[8] === "X" ||
+      board[2] === "X" && board[4] === "X" && board[6] === "X") {
+        console.log("Diagonal completed");
+        return "win";
+    }
+
+    else if(board[0] === "O" && board[4] === "O" && board[8] === "O" ||
+    board[2] === "O" && board[4] === "O" && board[6] === "O") {
+      console.log("Diagonal completed");
+      return "loss";
+    }
+  }
+
+
   const checkBoard = () => {
-    //Check Rows
-    if(board[0] === board[1] === board[2] === "X" ||
-      board[3] === board[4] === board[5] === "X" ||
-      board[6] === board[7] === board[8] === "X") {
-        return "Winner!";
+    if(checkRows() === "win" || checkColumns() === "win" || checkDiagonal() === "win") {
+      openResult("X");
     }
-    else if(board[0] === board[1] === board[2] === "X" ||
-      board[3] === board[4] === board[5] === "X" ||
-      board[6] === board[7] === board[8] === "X") {
-        return "Loser!";
+    else if(checkRows() === "loss" || checkColumns() === "loss" || checkDiagonal() === "loss") {
+      openResult("O");
     }
-
-    //Check Columns
-    if(board[0] === board[3] === board[6] === "X" ||
-      board[1] === board[4] === board[7] === "X" ||
-      board[2] === board[5] === board[8] === "X") {
-        return "Winner!";
-    }
-    else if(board[0] === board[3] === board[6] === "O" ||
-      board[1] === board[4] === board[7] === "O" ||
-      board[2] === board[5] === board[8] === "O") {
-        return "Loser!";
-    }
-
-    //Check Diagonal
-    if(board[0] === board[4] === board[8] === "X" ||
-      board[2] === board[4] === board[6] === "X") {
-        return "Winner!";
-    }
-    else if(board[0] === board[4] === board[8] === "O" ||
-    board[2] === board[4] === board[6] === "O") {
-        return "Loser!";
+    else {
+      if(board.includes("") === false) {
+        openResult("tie");
+      }
     }
   };
+
 
   const playBotRound = () => {
     let randomPosition = Math.floor((Math.random() * 9) + 1);
@@ -66,6 +115,7 @@ function App() {
     }
   };
 
+
   const selectSquare = (id) => {
     let boardSquare = document.getElementById(`board-square-${id}`);
 
@@ -85,21 +135,32 @@ function App() {
     }
   };
 
+
   const handleClick = () => {
     let startMenu = document.getElementById("start-menu");
     let gameUI = document.getElementById("game-ui");
 
+    console.log(startMenu);
+    console.log(gameUI);
     startMenu.style.display = "none";
     gameUI.style.display = "flex";
   };
 
+
   const handleReset = () => {
+    let gameUI = document.getElementById("game-ui");
+    let result = document.getElementById("result");
+
     for(let i = 0; i < board.length ; i++) {
       document.getElementById(`board-square-${i + 1}`).innerHTML = "";
       board[i] = "";
     }
+    result.style.display = "none";
+    gameUI.style.display = "flex";
     console.log(board);
   }
+
+
 
   return (
     <div className="wrapper">
@@ -109,6 +170,7 @@ function App() {
     </div>
   )
 };
+
 
 
 export default App;
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,7 +4,7 @@ import '../styles/Result.css';
 const Result = ({ handleReset }) => {
     return (
         <div id="result" className="result">
-            <h1>You're a Winner!</h1>
+            <h1 id="result-text"></h1>
             <button onClick= { handleReset }>Reset</button>
         </div>
     )

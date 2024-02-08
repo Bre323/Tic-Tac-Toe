96201ca9f5e9aa56908e208cd4c18feda0965ae4
Breno Rocha <brenopenarocha2@gmail.com>
2024-02-08 10:00:58 -0300

    playBotRound and checkBoard functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,87 @@ import React from "react";
 import StartMenu from './components/StartMenu.jsx';
 import Game from './components/Game.jsx';
 
+let board = ["", "", "", "", "", "", "", "", ""];
+
+
 function App() {
 
+  const checkBoard = () => {
+    //Check Rows
+    if(board[0] === board[1] === board[2] === "X" ||
+      board[3] === board[4] === board[5] === "X" ||
+      board[6] === board[7] === board[8] === "X") {
+        return "Winner!";
+    }
+    else if(board[0] === board[1] === board[2] === "X" ||
+      board[3] === board[4] === board[5] === "X" ||
+      board[6] === board[7] === board[8] === "X") {
+        return "Loser!";
+    }
+
+    //Check Columns
+    if(board[0] === board[3] === board[6] === "X" ||
+      board[1] === board[4] === board[7] === "X" ||
+      board[2] === board[5] === board[8] === "X") {
+        return "Winner!";
+    }
+    else if(board[0] === board[3] === board[6] === "O" ||
+      board[1] === board[4] === board[7] === "O" ||
+      board[2] === board[5] === board[8] === "O") {
+        return "Loser!";
+    }
+
+    //Check Diagonal
+    if(board[0] === board[4] === board[8] === "X" ||
+      board[2] === board[4] === board[6] === "X") {
+        return "Winner!";
+    }
+    else if(board[0] === board[4] === board[8] === "O" ||
+    board[2] === board[4] === board[6] === "O") {
+        return "Loser!";
+    }
+  };
+
+  const playBotRound = () => {
+    let randomPosition = Math.floor((Math.random() * 9) + 1);
+    let boardSquare = document.getElementById(`board-square-${randomPosition}`);
+
+    if(boardSquare.innerHTML == "") {
+      let symbol = document.createElement("p");
+      symbol.classList.add("symbol");
+      symbol.innerText = "O";
+
+      boardSquare.appendChild(symbol);
+      board[randomPosition - 1] = "O";
+      console.log(board);
+      checkBoard();
+    }
+    else {
+      if(board.includes("")) {
+        console.log("Not Empty!");
+        playBotRound();
+      }
+    }
+  };
+
   const selectSquare = (id) => {
-    let boardSquare = document.getElementById(id);
+    let boardSquare = document.getElementById(`board-square-${id}`);
 
-
-    //console.log(boardSquare.innerHTML);
     if(boardSquare.innerHTML == "") {
       let symbol = document.createElement("p");
       symbol.classList.add("symbol");
       symbol.innerText = "X";
 
       boardSquare.appendChild(symbol);
+      board[id - 1] = "X";
+      console.log(board);
+      checkBoard();
+      playBotRound();
     }
     else {
       console.log("you already clicked here!");
     }
-  }
+  };
 
   const handleClick = () => {
     let startMenu = document.getElementById("start-menu");
@@ -27,7 +90,7 @@ function App() {
 
     startMenu.style.display = "none";
     gameUI.style.display = "flex";
-  }
+  };
 
   return (
     <div className="wrapper">
@@ -35,6 +98,7 @@ function App() {
       <Game selectSquare={ selectSquare } />
     </div>
   )
-}
+};
 
-export default App
+
+export default App;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -9,16 +9,16 @@ const Game = ({ selectSquare }) => {
                 <p className="symbol">X</p>                
             </div>
 
-            <div className="game-board">
-                <div id="board-square-1" className="board-square" onClick={ () => selectSquare("board-square-1") }></div>
-                <div id="board-square-2" className="board-square" onClick={ () => selectSquare("board-square-2") }></div>
-                <div id="board-square-3" className="board-square" onClick={ () => selectSquare("board-square-3") }></div>
-                <div id="board-square-4" className="board-square" onClick={ () => selectSquare("board-square-4") }></div>
-                <div id="board-square-5" className="board-square" onClick={ () => selectSquare("board-square-5") }></div>
-                <div id="board-square-6" className="board-square" onClick={ () => selectSquare("board-square-6") }></div>
-                <div id="board-square-7" className="board-square" onClick={ () => selectSquare("board-square-7") }></div>
-                <div id="board-square-8" className="board-square" onClick={ () => selectSquare("board-square-8") }></div>
-                <div id="board-square-9" className="board-square" onClick={ () => selectSquare("board-square-9") }></div>
+            <div id="game-board" className="game-board">
+                <div id="board-square-1" className="board-square" onClick={ () => selectSquare(1) }></div>
+                <div id="board-square-2" className="board-square" onClick={ () => selectSquare(2) }></div>
+                <div id="board-square-3" className="board-square" onClick={ () => selectSquare(3) }></div>
+                <div id="board-square-4" className="board-square" onClick={ () => selectSquare(4) }></div>
+                <div id="board-square-5" className="board-square" onClick={ () => selectSquare(5) }></div>
+                <div id="board-square-6" className="board-square" onClick={ () => selectSquare(6) }></div>
+                <div id="board-square-7" className="board-square" onClick={ () => selectSquare(7) }></div>
+                <div id="board-square-8" className="board-square" onClick={ () => selectSquare(8) }></div>
+                <div id="board-square-9" className="board-square" onClick={ () => selectSquare(9) }></div>
             </div>
             
             <div className="player-symbol">
